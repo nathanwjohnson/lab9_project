1be431c595eccd2a378a3bb48706be23d6a95725
Trevor Reay <104921845+tdr42@users.noreply.github.com>
2022-11-30 19:38:33 -0700

    Create tronControl.h

---
+++ tronControl.h
@@ -0,0 +1,10 @@
+#ifndef TRONCONTROL
+#define TRONCONTROL
+
+// Initialize the game control logic
+void tronControl_init();
+
+// Tick the game control logic
+void tronControl_tick();
+
+#endif /* TRONCONTROL */
